9495928104c4e12663d4b6eb9905fa5dbc4d444d
axelice <axelice89@gmail.com>
2021-11-09 14:51:47 +0530

    it contain header and footer

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<div class="insert-post-ads1" style="margin-top:20px;">
+    
+</div>
+</div>
+<footer class="text-center">
+        <small>Copyright © <?php echo date("Y"); ?> Viku. All rights reserved.</small>
+    </footer>
+</body>
+
+</html>
